3b4a53637cd3e264c358cdab986a572f1abde882
Puskar-Roy <puskarroy600@gmail.com>
2024-04-15 18:33:09 +0530

    feat : config

---
+++ server/src/config/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DB_URL,
+  JWT_TOKEN: process.env.RANDOM_TOKEN,
+  SMTP_APP_PASSWORD: process.env.SMTP_APP_PASSWORD,
+  GEMINI_KEY: process.env.GEMINI_KEY,
+};
