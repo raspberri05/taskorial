cd546cf76000124451b4dd25c2eff977d1b8073b
ovodela <102893727+ovodela@users.noreply.github.com>
2024-04-15 21:28:05 -0400

    Fixed visuals of Delete Account page

--- react-auth/src/DeleteAccount.tsx
+++ react-auth/src/DeleteAccount.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Cookies from "universal-cookie";
 import axios from 'axios';
 
@@ -9,35 +9,36 @@ export const DeleteAccount: FC<{ token: string }> = (props) => {
 
   const deleteAccount = async () => {
     if (!token) {
-    setMessage('Token not found. Please login again.');
-    return;
+      setMessage('Token not found. Please login again.');
+      return;
     }
 
     const configuration = {
     method: "delete",
     url: `${process.env.REACT_APP_API_URL}delete-account`,
     headers: {
-        Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     },
     };
 
     axios(configuration)
     .then((response) => {
-        setMessage(response.data.message + "  Logging out...");
-        setTimeout(() => { window.location.href = "/login" }, 3000); // Redirect to login page after 3 seconds
-        const cookies = new Cookies();
-        cookies.remove("TOKEN", { path: "/" });
+      setMessage(response.data.message + " - Logging out...");
+      setTimeout(() => { window.location.href = "/login" }, 3000); // Redirect to login page after 3 seconds
+      const cookies = new Cookies();
+      cookies.remove("TOKEN", { path: "/" });
     })
     .catch((error) => {
-        setMessage('Error deleting account');
-        console.log(error);
+      setMessage('Error deleting account');
+      console.log(error);
     });
   };
 
   return (
-    <div>
-      <button onClick={deleteAccount}>Delete Account</button>
+    <Container>
+      <p>Please confirm to delete your account (all of your tasks will also be deleted)</p>  
+      <Button variant="primary" onClick={deleteAccount}>Delete Account</Button>
       <p>{message}</p>
-    </div>
+    </Container>
   );
 };
