0ec42d20e13a8c56dbb0798e3481851d0bdd5b13
ravishxt <ravishxt@gmail.com>
2024-04-25 12:52:22 +0530

    Fix: Replaced any keyword with appropriate types

--- react-auth/src/components/ProtectedRoutes.tsx
+++ react-auth/src/components/ProtectedRoutes.tsx
@@ -4,8 +4,12 @@ import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 
-export const ProtectedRoutes: FC<any> = ({ children }) => {
+/**
+ * A higher-order component (HOC) that wraps around a child component.
+ * It takes a 'children' prop representing the protected content to be rendered.
+ */
+export const ProtectedRoutes: FC<{ children: React.ReactNode }> = ({ children }) => {
   const token = cookies.get("TOKEN");
-  if (token) return children;
+  if (token) return (children as React.ReactElement);
   else return <Navigate to="/login" />;
 };
