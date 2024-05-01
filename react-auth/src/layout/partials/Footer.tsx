04775bf3be768db870378edbd4baaf7c5496e7d9
user36542654 <zqfk52@durham.ac.uk>
2024-05-01 08:24:32 +0100

    reseting footer and navbar back to original

--- react-auth/src/layout/partials/Footer.tsx
+++ react-auth/src/layout/partials/Footer.tsx
@@ -5,80 +5,7 @@ import "../../main.css";
  * Functional component representing footer of app
  * @returns JSX Element containing footer
  */
-function Footer() {
-  /**
-   * Function to display legal links in footer of app
-   * @returns JSX Element containing legal links
-   */
-  const legalLinks = () => (
-    <div className="col-6 col-lg-2 mb-3">
-      <h5 className="fs-5">Legal</h5>
-      <ul className="list-unstyled">
-        <li className="mb-2">
-          <Link to="/terms">Terms of Use</Link>
-        </li>
-        <li className="mb-2">
-          <Link to="/privacy">Privacy Policy</Link>
-        </li>
-      </ul>
-    </div>
-  );
-  /**
-   * Function to display community links in footer of app
-   * @returns JSX Element containing community links
-  */
-  const communityLinks = () => (
-    <div className="col-6 col-lg-2 mb-3">
-      <h5 className="fs-5">Community</h5>
-      <ul className="list-unstyled">
-        <li className="mb-2">
-          <a
-            href="https://github.com/raspberri05/taskorial"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github" /> Github
-          </a>
-        </li>
-        <li className="mb-2">
-          <a
-            href="https://github.com/sponsors/raspberri05"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fas fa-hand-holding-usd" /> Sponsor
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-
-  /**
-   * Function to display contact links in footer of app
-   * @returns JSX Element containing contact links
-   */
-  const contactLinks = () => (
-    <div className="col-6 col-lg-2 mb-3">
-      <h5 className="fs-5">Contact Us</h5>
-      <ul className="list-unstyled">
-        <li className="mb-2">
-          <a
-            href="mailto:support@taskorial.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fas fa-envelope" /> support@taskorial.com
-          </a>
-        </li>
-        <li className="mb-2">
-          <Link to="/feedback">
-            <i className="fas fa-comment-dots" /> Feedback Form
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-
+export default function Footer() {
   return (
     <footer className="bd-footer py-4 py-md-5 mt-5 footer-link footer">
       <div className="container py-4 py-md-5 px-4 px-md-3 border-top">
@@ -96,9 +23,59 @@ function Footer() {
               </li>
             </ul>
           </div>
-          {legalLinks()}
-          {communityLinks()}
-          {contactLinks()}
+          <div className="col-6 col-lg-2 mb-3">
+            <h5 className="fs-5">Legal</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <Link to="/terms">Terms of Use</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/privacy">Privacy Policy</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 col-lg-2 mb-3">
+            <h5 className="fs-5">Community</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <a
+                  href="https://github.com/raspberri05/taskorial"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-github" /> Github
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="https://github.com/sponsors/raspberri05"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fas fa-hand-holding-usd" /> Sponsor
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 col-lg-2 mb-3">
+            <h5 className="fs-5">Contact Us</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <a
+                  href="mailto:vedant.singhania@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fas fa-envelope" /> vedant.singhania@gmail.com
+                </a>
+              </li>
+              <li className="mb-2">
+                <Link to="/feedback">
+                  <i className="fas fa-comment-dots" /> Feedback Form
+                </Link>
+              </li>
+            </ul>
+          </div>
           <div className="col-lg-3 offset-lg-1 mb-3">
             <h5>
               <img
@@ -111,7 +88,7 @@ function Footer() {
             <ul className="list-unstyled">
               <li className="mb-2">Copyright 2024 Taskorial</li>
               <li className="mb-2">
-                Created by{' '}
+                Created by{" "}
                 <a
                   href="https://vedantsinghania.com"
                   target="_blank"
@@ -127,5 +104,3 @@ function Footer() {
     </footer>
   );
 }
-
-export default Footer;
--- react-auth/src/layout/partials/NavBar.tsx
+++ react-auth/src/layout/partials/NavBar.tsx
@@ -46,7 +46,7 @@ export const NavBar: FC<object> = () => {
               fill="currentColor"
               className="bi bi-list"
               viewBox="0 0 16 16"
-            >
+              >
               <path
                 fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
