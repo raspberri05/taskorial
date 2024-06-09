71152506cc911e1363c50418db2b9f924415007a
Vedant Singhania <vedant.singhania@gmail.com>
2024-06-08 19:12:04 -0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,23 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  const authHeader = req.headers.get('Authorization');
+  const path = req.nextUrl.pathname;
+  if (path.includes('/api')) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return Response.json(
+        { success: false, message: 'authentication failed' },
+        { status: 401 }
+      )
+    }
+  }
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
